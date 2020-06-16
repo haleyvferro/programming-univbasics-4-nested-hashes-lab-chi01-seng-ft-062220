15b23b4abc677ae279d72eae7ed5db6cd3f89f2b
Haley Ferro <haleyvferro@gmail.com>
2020-06-16 01:42:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,8 +16,8 @@ def hopper
       }
     }
     
-programmer_hash.shift
 end
+
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
